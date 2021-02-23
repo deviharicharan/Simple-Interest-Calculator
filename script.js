00b952e9f04cca40057fe0dd35f4e6e18fb2b5d1
BHARMAVATH <haricharan12321@gmail.com>
2021-02-23 22:02:41 +0530

    created the base files

---
+++ script.js
@@ -0,0 +1,34 @@
+function compute() {
+    var principalBox = document.getElementById('p');
+    var p = principalBox.value;
+    var t = document.getElementById('t').value;
+    var r = document.getElementById('r').value;
+    var a = new Date();
+    date = a.getFullYear();
+    if (p <= 0) {
+        alert("Please enter the valid Amount!!");
+        principalBox.focus();
+        principalBox.value = "";
+    } else {
+        var x = (p * t * r) / 100;
+        var year = parseInt(t);
+        var tf = date + year;
+        document.getElementById("result").innerHTML =
+            "If you deposit <b class='color'>" +
+            p +
+            "</b>, <br> at an interest rate of <b class='color'>" +
+            r +
+            "%</b>. <br>You will receive an amount of <b class='color'>" +
+            x +
+            "</b>, <br>in the year <b class='color'> " +
+            tf + "</b>.";
+    }
+}
+
+function sliderChange(val) {
+    document.getElementById("sliderVal").innerHTML = val + "%";
+    // compute();
+
+}
+var button = document.getElementById('btn');
+button.addEventListener('click', compute);
